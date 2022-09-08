dd6554a2a3d54d6b553b39e6163b7766264a626d
Vikash <vikaspatelp83@gmail.com>
2022-09-09 01:37:20 +0530

    add day1:roman-to-integer

---
+++ day1/roman-to-int.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# LEETCODE #13
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        hm = {
+            "I" : 1,
+            "V" : 5,
+            "X" : 10,
+            "L" : 50,
+            "C" : 100,
+            "D" : 500,
+            "M" : 1000
+        }
+
+        n = len(s)
+        res = hm[s[n-1]]
+        for i in range(n-2, -1, -1):
+            left = hm[s[i]]
+            right = hm[s[i+1]]
+
+            if left >= right:
+                res += left
+            else:
+                res -= left
+
+        return res
+
