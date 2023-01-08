81b0dbfd2189d6f30cb40c32a589b6ab50fb637d
Aakash Patel <akashpatelp83@gmail.com>
2023-01-08 09:03:57 +0530

    Add: Day 11 -Leetcode Climbing Stairs.py

---
+++ day11/Climbing Stairs.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+# LEETCODE  #70. Climbing Stairs
+
+
+'''
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+
+Example 2:
+
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+'''
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        a = 0
+        b = 1
+
+        for i in range(n):
+            tmp = a
+            a = b
+            b = tmp + b
+
+        return b
